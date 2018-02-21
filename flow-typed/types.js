2308b7cc5c33cb83c2d2bca89b8d6544538c1ea8
zaggen <xpynesoul@gmail.com>
2018-02-21 18:00:00 +0100

    Fixes issue with last commit

--- flow-typed/types.js
+++ flow-typed/types.js
@@ -11,25 +11,25 @@ declare type $ReduxActionsMap = {
 declare type Map<S: Object> = {|
   get: (k: $Keys<S>) => any,
   getIn: (path: any[], dfltValue: any) => any,
-  set: (k: $Keys<S>, v: $Values<S>) => MapShape<S>,
-  setIn: (path: (string | number)[], fn: Function) => MapShape<S>,
-  merge: (d: S) => MapShape<S>,
-  update: (k: string, fn: Function) => MapShape<S>,
-  remove: (k: string) => MapShape<S>,
+  set: (k: $Keys<S>, v: $Values<S>) => Map<S>,
+  setIn: (path: (string | number)[], fn: Function) => Map<S>,
+  merge: (d: S) => Map<S>,
+  update: (k: string, fn: Function) => Map<S>,
+  remove: (k: string) => Map<S>,
   toJS: () => S
 |}
 
 declare type List<S: any> = {|
   get: (i: number) => any,
   getIn: (path: any[], dfltValue: any) => any,
-  set: (i: number, v: $Values<S>) => ListShape<S>,
-  setIn: (path: any[], Function) => ListShape<S>,
-  update: (i: number, Function) => ListShape<S>,
-  updateIn: (path: string[], Function) => ListShape<S>,
-  remove: (i: number) => ListShape<S>,
-  find: (fn: Function) => ListShape<S>,
+  set: (i: number, v: $Values<S>) => List<S>,
+  setIn: (path: any[], Function) => List<S>,
+  update: (i: number, Function) => List<S>,
+  updateIn: (path: string[], Function) => List<S>,
+  remove: (i: number) => List<S>,
+  find: (fn: Function) => List<S>,
   findIndex: (fn: Function) => number,
-  push: (val: S) => ListShape<S>,
+  push: (val: S) => List<S>,
   toJS: () => S[]
 |}
 
